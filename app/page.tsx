4cb8598c2cfd109f2ddb430030cd8a151f55a07f
DaveSan2021 <54080390+DaveSan2021@users.noreply.github.com>
2024-09-04 16:01:23 -0400

    added packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import MainNavbar from "./components/ui/MainNavbar";
+//import MainNavbar from "./components/ui/MainNavbar";
 
 
 export default function Home() {
