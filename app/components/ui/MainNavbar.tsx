10d845ed8eab9aa1f65f00ee497245e54786bef7
DaveSan2021 <54080390+DaveSan2021@users.noreply.github.com>
2024-09-05 10:14:59 -0400

    updated navbar width

--- app/components/ui/MainNavbar.tsx
+++ app/components/ui/MainNavbar.tsx
@@ -1,3 +1,4 @@
+
 import {  
     Navbar,  
     NavbarBrand,   
@@ -13,23 +14,23 @@ import {Button, ButtonGroup} from "@nextui-org/button";
 const MainNavbar = async () => {
 
     return (
-        <div className="text-black flex">
-            <Navbar>
+        <div className="text-black">
+            <Navbar maxWidth={'xl'} isBordered> 
             <NavbarContent className="hidden sm:flex gap-4" justify="start">
-                <NavbarItem> 
+                <NavbarItem isActive> 
                     <Link href="/"> <Button>Home</Button> </Link>
                 </NavbarItem>
-                <NavbarItem> 
+                <NavbarItem isActive> 
                     <Link href="/resume"> <Button>Resume</Button> </Link>
                 </NavbarItem>
-                <NavbarItem> 
+                <NavbarItem isActive> 
                     <Link href="/projects"> <Button>Projects</Button> </Link>
                 </NavbarItem>
                 
             </NavbarContent>
             
             <NavbarContent className="" justify="end">
-                <NavbarItem> 
+                <NavbarItem isActive> 
                     <Link href="/contacts"> <Button color="primary"> Contact Me </Button></Link>
                 </NavbarItem>
             </NavbarContent>
